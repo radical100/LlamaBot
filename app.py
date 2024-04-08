f5fd4ea5b9d7bdf401ba4dd9e2ef315c2e2c3b05
Pablo Espinosa <164902961+pabloespi218@users.noreply.github.com>
2024-04-08 18:04:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,12 @@ service_context = ServiceContext.from_defaults(
 import chromadb
 from llama_index.vector_stores import ChromaVectorStore
 
-chroma_client = chromadb.Client(path="./assets/vectorestores/chroma")
+from chromadb.config import Settings
+chroma_client = chromadb.Client(Settings(
+    chroma_db_impl="duckdb+parquet",
+    persist_directory="./assets/vectorestores/chroma" # Optional, defaults to .chromadb/ in the current directory
+))
+
 collection_name = "host_index"
 chroma_collection = chroma_client.get_collection(collection_name)
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
